372fcb2bd404f71ff5613b57d4a6f77fcc41f24e
Zach Waugh <zwaugh@gmail.com>
2013-04-05 16:03:37 -0400

    Add Rdio service. Start some work on making the parsing more robust

---
+++ Pluck/PLKRdioService.h
@@ -0,0 +1,13 @@
+//
+//  PLKRdioService.h
+//  PluckDemo
+//
+//  Created by Zach Waugh on 4/5/13.
+//  Copyright (c) 2013 Zach Waugh. All rights reserved.
+//
+
+#import "PLKOEmbedService.h"
+
+@interface PLKRdioService : PLKOEmbedService
+
+@end
---
+++ Pluck/PLKRdioService.m
@@ -0,0 +1,13 @@
+//
+//  PLKRdioService.m
+//  PluckDemo
+//
+//  Created by Zach Waugh on 4/5/13.
+//  Copyright (c) 2013 Zach Waugh. All rights reserved.
+//
+
+#import "PLKRdioService.h"
+
+@implementation PLKRdioService
+
+@end
